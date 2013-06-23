c66b59bf4492e2a94eef0721f533cd0ed0143237
Tidus <tidus1215@gmail.com>
2013-06-24 00:13:15 +0800

    Start menu now is OK

--- touhou-all-star/src/touhou/all/star/screens/GameMenu.java
+++ touhou-all-star/src/touhou/all/star/screens/GameMenu.java
@@ -20,45 +20,52 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 public class GameMenu extends GameScreen {
 	private final SpriteBatch spriteBatch;
+	private Image background;
 	private Image background0;
 	private Image background1;
 	private Image background2;
 	private Image background3;
 	private Image background4;
-	private Stage stage; 
-    private Texture texture;
-    
-    private float alpha=0;
-    private float menuDuration=0;
-    private boolean isDone = false;
-    
-    private RotateByAction rotateAction;
-    private MoveByAction moveAction;
-    private ArrayList<Flower> flowerArray = new ArrayList<Flower>();
-    private ArrayList<Float> flowerDuration = new ArrayList<Float>();
-    
+	private Image manual1;
+	private Image manual2;
+	private Stage stage;
+	private Texture texture;
+
+	private boolean enableClick = false;
+	private int click = 0;
+	private float click1time = 0;
+	private float click2time = 0;
+	private float alpha = 0;
+	private float menuDuration = 0;
+	private boolean isDone = false;
+
+	private RotateByAction rotateAction;
+	private MoveByAction moveAction;
+	private ArrayList<Flower> flowerArray = new ArrayList<Flower>();
+	private ArrayList<Float> flowerDuration = new ArrayList<Float>();
+
 	public static final String FONT_CHARACTERS = "東东方全明星上海爱丽丝幻乐团弹幕过敏者注意伪本游戏根据网的视频制作而来很多面都不足若有侵犯到版权联系我处理请包涵：，。";
 	public static final String mainTitle = "东方弹幕All Star(伪)";
 	public static final String version = "Touhou Barrage All Star (fake)";
 	public static final String caution1 = "注：本游戏根据网上东方全明星的视频制作而来，很多方面都有不足。请多多包涵";
 	public static final String caution2 = "若有侵犯到版权，十分抱歉，请联系我处理^_^";
-	
+
 	FreeTypeFontGenerator generator;
 	FreeTypeBitmapFontData fontData;
-	private BitmapFont freetypeBitmapFont; 
+	private BitmapFont freetypeBitmapFont;
 	BitmapFont font1;
 	BitmapFont font2;
 	BitmapFont font3;
 	BitmapFont font4;
-	
 
 	public GameMenu() {
 		Texture.setEnforcePotImages(false);
 		spriteBatch = new SpriteBatch();
-		
+
 		texture = new Texture(Gdx.files.internal("background1_2.png"));
-		background0 = new Image (texture);
-		
+		background = new Image(texture);
+		texture = new Texture(Gdx.files.internal("start_back_0.png"));
+		background0 = new Image(texture);
 		texture = new Texture(Gdx.files.internal("start_back_1.png"));
 		background1 = new Image(texture);
 		texture = new Texture(Gdx.files.internal("start_back_2.png"));
@@ -67,41 +74,61 @@ public class GameMenu extends GameScreen {
 		background3 = new Image(texture);
 		texture = new Texture(Gdx.files.internal("start_back_4.png"));
 		background4 = new Image(texture);
-		
+		texture = new Texture(Gdx.files.internal("manual_1.png"));
+		manual1 = new Image(texture);
+		texture = new Texture(Gdx.files.internal("manual_2.png"));
+		manual2 = new Image(texture);
+
+		background0.setColor(1, 1, 1, 0);
 		background1.setColor(1, 1, 1, 0);
 		background2.setColor(1, 1, 1, 0);
 		background3.setColor(1, 1, 1, 0);
 		background4.setColor(1, 1, 1, 0);
-		
+		manual1.setColor(1, 1, 1, 0);
+		manual2.setColor(1, 1, 1, 0);
+
 		generator = new FreeTypeFontGenerator(Gdx.files.internal("font2.ttf"));
-		fontData = generator.generateData(55, FreeTypeFontGenerator.DEFAULT_CHARS + FONT_CHARACTERS, false);
+		fontData = generator.generateData(55,
+				FreeTypeFontGenerator.DEFAULT_CHARS + FONT_CHARACTERS, false);
 		font1 = new BitmapFont(fontData, fontData.getTextureRegion(), false);
 		font1.setColor(0.6f, 0.6f, 1, 0);
-		
-		fontData = generator.generateData(25, FreeTypeFontGenerator.DEFAULT_CHARS + FONT_CHARACTERS, false);
+
+		fontData = generator.generateData(25,
+				FreeTypeFontGenerator.DEFAULT_CHARS + FONT_CHARACTERS, false);
 		font2 = new BitmapFont(fontData, fontData.getTextureRegion(), false);
-		
-		fontData = generator.generateData(25, FreeTypeFontGenerator.DEFAULT_CHARS + FONT_CHARACTERS, false);
+
+		fontData = generator.generateData(25,
+				FreeTypeFontGenerator.DEFAULT_CHARS + FONT_CHARACTERS, false);
 		font3 = new BitmapFont(fontData, fontData.getTextureRegion(), false);
-		
-		fontData = generator.generateData(30, FreeTypeFontGenerator.DEFAULT_CHARS + FONT_CHARACTERS, false);
+
+		fontData = generator.generateData(30,
+				FreeTypeFontGenerator.DEFAULT_CHARS + FONT_CHARACTERS, false);
 		font4 = new BitmapFont(fontData, fontData.getTextureRegion(), false);
 		font4.setColor(0.3f, 0.56f, 0.7f, 1f);
-		
+
 		generator.dispose();
-		
-		stage = new Stage(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), 
-                true); 
-		
-		 Gdx.input.setInputProcessor(stage); 
+
+		stage = new Stage(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(),
+				true);
+
+		Gdx.input.setInputProcessor(stage);
 	}
 
 	@Override
-	public void update (float delta) {
-		if (menuDuration > 4f) {
+	public void update(float delta) {
+		if (enableClick) {
 			if (Gdx.input.justTouched() || Gdx.input.isKeyPressed(Keys.ENTER)
-					|| Gdx.input.isKeyPressed(Keys.SPACE) || menuDuration > 8f) {
-				isDone = true;
+					|| Gdx.input.isKeyPressed(Keys.SPACE)) {
+				click++;
+				if (click == 1) {
+					click1time = alpha;
+				}
+				if (click == 2) {
+					click2time = alpha;
+				}
+				if(click == 3){
+					isDone = true;
+				}
 			}
 		}
 	}
@@ -110,74 +137,115 @@ public class GameMenu extends GameScreen {
 	public boolean isDone() {
 		return isDone;
 	}
-	
+
 	@Override
 	public void draw(float delta) {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
+
 		menuDuration += delta;
 		alpha += delta;
-		
-		if(background1.getColor().a<1){
-			if(alpha>1)
-				alpha=1;
-			background1.setColor(1, 1, 1, alpha);
-		}else{
-			if(background2.getColor().a<1){
-				if(alpha>2)
-					alpha=2;
-				background2.setColor(1, 1, 1, alpha-1);
-			}else{
-				if(background3.getColor().a<1){
-					if(alpha>3)
-						alpha=3;
-					background3.setColor(1, 1, 1, alpha-2);
-				}else{
-					if(background4.getColor().a<1){
-						if(alpha>4)
-							alpha=4;
-						background4.setColor(1, 1, 1, alpha-3);
+
+		if (background0.getColor().a < 1) {
+			if (alpha > 1)
+				alpha = 1;
+			background0.setColor(1, 1, 1, alpha);
+		} else {
+			if (background1.getColor().a < 1) {
+				if (alpha > 2)
+					alpha = 2;
+				background1.setColor(1, 1, 1, alpha - 1);
+			} else {
+				if (background2.getColor().a < 1) {
+					if (alpha > 3)
+						alpha = 3;
+					background2.setColor(1, 1, 1, alpha - 2);
+				} else {
+					if (background3.getColor().a < 1) {
+						if (alpha > 4)
+							alpha = 4;
+						background3.setColor(1, 1, 1, alpha - 3);
+					} else {
+						if (background4.getColor().a < 1) {
+							if (alpha > 5)
+								alpha = 5;
+							background4.setColor(1, 1, 1, alpha - 4);
+						}
 					}
 				}
 			}
 		}
+
+		if (click==1) {
+			if(alpha-click1time < 1){
+				manual1.setColor(1, 1, 1, alpha-click1time);
+				enableClick = false;
+			}else{
+				enableClick = true;
+			}
+			
+		}
+
+		if (click==2) {
+			if(alpha-click2time < 1){
+				enableClick = false;
+				manual1.setColor(1, 1, 1, 1-(alpha-click2time));
+				manual2.setColor(1, 1, 1, alpha-click2time);
+			}else{
+				manual1.setColor(1, 1, 1, 0);
+				manual2.setColor(1, 1, 1, 1);
+				enableClick = true;
+			}
+		}
+
+		stage.addActor(background);
 		stage.addActor(background0);
 		stage.addActor(background1);
 		stage.addActor(background2);
 		stage.addActor(background3);
 		stage.addActor(background4);
+
+		stage.addActor(manual1);
+		stage.addActor(manual2);
+
 		stage.act(Gdx.graphics.getDeltaTime());
 		stage.draw();
-//		viewMatrix.setToOrtho2D(0, 0, 480, 320);
-//		spriteBatch.setProjectionMatrix(viewMatrix);
-//		spriteBatch.setTransformMatrix(transformMatrix);
+
+		// viewMatrix.setToOrtho2D(0, 0, 480, 320);
+		// spriteBatch.setProjectionMatrix(viewMatrix);
+		// spriteBatch.setTransformMatrix(transformMatrix);
+
 		spriteBatch.begin();
 		spriteBatch.setColor(Color.WHITE);
-//		spriteBatch.enableBlending();
+		// spriteBatch.enableBlending();
 
-		if(alpha>=4)
-		{
-			if(alpha<5){
-				font1.setColor(0.6f, 0.6f, 1, alpha-4);
-				font2.setColor(1,1,1,alpha-4);
-			}else if(alpha<6){
+		if (alpha >= 5) {  // background pic is OK
+			if (alpha < 6) { // then show the title
+				font1.setColor(0.6f, 0.6f, 1, alpha - 5);
+				font2.setColor(1, 1, 1, alpha - 5);
+			} else if (alpha <= 7){
 				font1.setColor(0.6f, 0.6f, 1, 1);
-				font2.setColor(1,1,1,1);
+				font2.setColor(1, 1, 1, 1);
 				
+				enableClick = true;
 			}
-			font1.draw(spriteBatch, mainTitle, 260, 460); // left bottom is (0,0)
+			if (click==1) {
+				if(alpha-click1time < 0.8){
+					enableClick = false;
+					font1.setColor(0.6f, 0.6f, 1, 1-(alpha-click1time)*0.8f);
+					font2.setColor(1, 1, 1, 1-(alpha-click1time)*0.8f);
+				}else{
+					enableClick = true;
+				}
+			}
+			font1.draw(spriteBatch, mainTitle, 260, 460); // left bottom is(0,0)
 			font2.draw(spriteBatch, version, 320, 400);
-			
-			font3.draw(spriteBatch, caution1, 80, 160);
-//			font3.draw(spriteBatch, caution2, 80, 100);
 		}
-//		font4.draw(spriteBatch, loading, 800, 120);
+
 		spriteBatch.end();
 	}
-	
-	
-	public void dispose () {
+
+	public void dispose() {
 		spriteBatch.dispose();
 		texture.dispose();
 		font1.dispose();
@@ -185,6 +253,3 @@ public class GameMenu extends GameScreen {
 		font3.dispose();
 	}
 }
-
-
-
